487a9d1312546b5e9117f22c5a131fe233e2d64a
Your Name <jmzd.developer94@gmail.com>
2021-08-26 22:29:53 +0430

    Profile part 1 : viewModel + layout

---
+++ app/src/main/java/com/jmzd/ghazal/kotlinmvvmonlineshop/model/DataModel_Profile.kt
@@ -0,0 +1,16 @@
+package com.jmzd.ghazal.kotlinmvvmonlineshop.model
+
+class DataModel_Profile (
+    val name: String,
+    val mobile :String,
+    val email :String
+)
+
+//http://localhost/shop/user_info.php?user_id=7
+//[
+//    {
+//        "name": "ghazal",
+//        "mobile": "0910",
+//        "email": "ghazal@gmail.com"
+//    }
+//]
