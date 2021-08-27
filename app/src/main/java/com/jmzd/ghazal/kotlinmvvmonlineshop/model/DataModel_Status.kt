ea330116d24a0cab96bdaffbd9956fa79b1655a2
Your Name <jmzd.developer94@gmail.com>
2021-08-28 01:11:06 +0430

    json

--- app/src/main/java/com/jmzd/ghazal/kotlinmvvmonlineshop/model/DataModel_Status.kt
+++ app/src/main/java/com/jmzd/ghazal/kotlinmvvmonlineshop/model/DataModel_Status.kt
@@ -6,3 +6,13 @@ class DataModel_Status (
 )
 // login & register :
 //خروچی فایل php یک status و یک id می باشد.
+//http://localhost/shop/login.php?mobile=0910&pass=1234
+//{
+//    "status": "ok",
+//    "user_id": "7",
+//    "user_name": "ghazal"
+//}
+
+//{
+//    "status": "error"
+//}
